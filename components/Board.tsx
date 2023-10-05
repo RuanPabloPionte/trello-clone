3c51249eb4e3c189439173dcab734f2fe9bb2933
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-05 14:40:59 -0300

    creation of the Board component

---
+++ components/Board.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useBoardStore } from "@/store/BoardStore";
+import { useEffect } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+function Board() {
+  const getBoard = useBoardStore((state) => state.getBoard);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+  return (
+    <h1>hello</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div>reder ALL THE columns</div>}
+    //   </Droppable>
+    // </DragDropContext>
+  );  
+}
+
+export default Board;
