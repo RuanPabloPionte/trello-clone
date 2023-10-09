fd3a17df2fabce50d18a927cee52b88b8bf57e02
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-09 09:29:13 -0300

    implement function  updateTodoInDB

--- components/Board.tsx
+++ components/Board.tsx
@@ -5,11 +5,14 @@ import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import Column from "./Column";
 
 function Board() {
-  const [board, getBoard, setBoardState] = useBoardStore((state) => [
-    state.board,
-    state.getBoard,
-    state.setBoardState,
-  ]);
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
+    (state) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateTodoInDB,
+    ]
+  );
 
   useEffect(() => {
     getBoard();
@@ -61,7 +64,6 @@ function Board() {
         id: startCol.id,
         todos: newTodos,
       };
-      console.log(newCol);
 
       const newColumns = new Map(board.columns);
       newColumns.set(startCol.id, newCol);
@@ -83,6 +85,7 @@ function Board() {
         todos: finishTodos,
       });
 
+      updateTodoInDB(todoMoved, finishCol.id);
       setBoardState({ ...board, columns: newColumns });
     }
   };
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,3 +1,4 @@
+import { databases } from "@/appwrite";
 import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumn";
 import { create } from "zustand";
 
@@ -5,6 +6,7 @@ type BoardStore = {
   board: Board;
   getBoard: () => void;
   setBoardState: (board: Board) => void;
+  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
 };
 
 export const useBoardStore = create<BoardStore>((set) => ({
@@ -16,4 +18,16 @@ export const useBoardStore = create<BoardStore>((set) => ({
     set({ board });
   },
   setBoardState: (board) => set({ board }),
+
+  updateTodoInDB: async (todo: Todo, columnId: TypedColumn) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
+  },
 }));
