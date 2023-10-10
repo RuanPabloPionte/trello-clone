f42cc6c5121790e1e70b01dc6ca0da1c50fd6646
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-10 09:51:57 -0300

    implement deleteTask function

--- components/Header.tsx
+++ components/Header.tsx
@@ -60,12 +60,12 @@ function Header() {
         </div>
       </div>
 
-      <div className="flex items-center justify-center px-5 md:py-5">
+      {/* <div className="flex items-center justify-center px-5 md:py-5">
         <p className="flex items-center text-sm  p-5 py-5 font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
           <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
           O famoso chat está resumido seu dia
         </p>
-      </div>
+      </div> */}
     </header>
   );
 }
