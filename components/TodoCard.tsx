e289beaaefa6e7669c66dc176b75aafa937972e0
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-10 18:45:42 -0300

    implement the modal

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useBoardStore } from "@/store/BoardStore";
 import { XCircleIcon } from "@heroicons/react/20/solid";
 import {
   DraggableProvidedDragHandleProps,
@@ -21,6 +22,7 @@ const TodoCard = ({
   draggableProps,
   dragHandleProps,
 }: Props) => {
+  const [deleteTask] = useBoardStore((state) => [state.deleteTask]);
   return (
     <div
       className="bg-white rounded-md space-y-2 drop-shadow-md"
@@ -30,7 +32,10 @@ const TodoCard = ({
     >
       <div className="flex justify-between items-center p-5">
         <p>{todo.title}</p>
-        <button className="text-red-500 hover:text-red-600">
+        <button
+          onClick={() => deleteTask(index, todo, id)}
+          className="text-red-500 hover:text-red-600"
+        >
           <XCircleIcon className="ml-5 h-8 w-8" />
         </button>
       </div>
